7fc63d6072871236987f29fce42a009045d01333
Bharat Kunwar <brtknr@bath.edu>
2019-10-07 04:57:02 -0500

    Move flavors and image to variables

--- k8s.tf
+++ k8s.tf
@@ -24,17 +24,27 @@ variable "keypair_name" {
   default = "default"
 }
 
+variable "image_name" {
+  type = string
+  #default = "FedoraAtomic29-20190708"
+  default = "Fedora-Atomic-27-20180419.0.x86_64"
+}
+
+variable "flavor_name" {
+  type = string
+  default = "C6420-Xeon6148-192"
+}
+
 provider "openstack" {
   cloud = "eiffel"
 }
 
 resource "openstack_containerinfra_clustertemplate_v1" "kubernetes_template" {
   name                  = "k8s"
-  #image                 = "FedoraAtomic29-20190708"
-  image                 = "Fedora-Atomic-27-20180419.0.x86_64"
+  image                 = "${var.image_name}"
   coe                   = "kubernetes"
-  flavor                = "C6420-Xeon6148-192"
-  master_flavor         = "C6420-Xeon6148-192"
+  flavor                = "${var.flavor_name}"
+  master_flavor         = "${var.flavor_name}"
   docker_storage_driver = "overlay2"
   network_driver        = "flannel"
   server_type           = "vm"
@@ -44,6 +54,17 @@ resource "openstack_containerinfra_clustertemplate_v1" "kubernetes_template" {
   master_lb_enabled     = false
   public                = true
   floating_ip_enabled   = false
+  labels = {
+    cgroup_driver="cgroupfs"
+    ingress_controller="traefik"
+    tiller_enabled="true"
+    tiller_tag="v2.14.3"
+    monitoring_enabled="true"
+    #kube_tag="v1.15.4" after upgrading to 8.1.0
+    kube_tag="v1.14.6"
+    cloud_provider_tag="v1.14.0"
+    heat_container_agent_tag="train-dev"
+  }
 }
 
 resource "openstack_containerinfra_cluster_v1" "cluster" {
@@ -52,14 +73,4 @@ resource "openstack_containerinfra_cluster_v1" "cluster" {
   master_count         = 1
   node_count           = 3
   keypair              = "${var.keypair_name}"
-  labels = {
-    cgroup_driver="cgroupfs"
-    ingress_controller="traefik"
-    tiller_enabled="true"
-    tiller_tag="v2.14.3"
-    monitoring_enabled="true"
-    kube_tag="v1.14.6"
-    cloud_provider_tag="v1.14.0"
-    heat_container_agent_tag="train-dev"
-  }
 }
